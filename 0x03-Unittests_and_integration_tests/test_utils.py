9de7399520005519870823faf2018a68be3b1b42
silgenius <olutademartin@gmail.com>
2024-11-04 13:17:39 +0000

    Fix error in code

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,7 +28,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
-    def test_accesss_nested_map(
+    def test_access_nested_map(
             self, nested_map: Mapping,
             path: Sequence,
             expected: Any
