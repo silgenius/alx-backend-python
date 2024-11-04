85eae3928de2b69374bcb658838d4840259758f0
silgenius <olutademartin@gmail.com>
2024-11-04 16:18:26 +0000

    Implement solution to task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,9 @@ from typing import (
     Sequence,
     Any,
 )
+from unittest.mock import patch, Mock
+import requests
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -48,3 +51,29 @@ class TestAccessNestedMap(unittest.TestCase):
         behavior of the nested map access method."""
         with self.assertRaises(error_type):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    validate the behavior of the utils.get_json function,
+    ensuring it returns the expected results under various conditions.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    def test_get_json(self, test_url, payload):
+        """
+        This method tests the utils.get_json function to verify that
+        it correctly retrieves and returns JSON data as intended
+        """
+        with patch('requests.get') as mock_request:
+            mock_response = Mock()
+            mock_response.json.return_value = payload
+            mock_request.return_value = mock_response
+
+            result = get_json(test_url)
+
+            mock_request.assert_called_once_with(test_url)
+            self.assertEqual(result, payload)
