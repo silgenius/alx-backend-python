5e512b1acc31f4f1d7fd8379fdc40ad5840a38ac
silgenius <olutademartin@gmail.com>
2024-11-05 12:35:06 +0000

    Fix bug in file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -13,6 +13,7 @@ from unittest.mock import (
     Mock,
     MagicMock,
 )
+from typing import Dict
 
 
 class TestiGithubOrgClient(unittest.TestCase):
@@ -26,16 +27,18 @@ class TestiGithubOrgClient(unittest.TestCase):
         ('abc', {"data": "some_data"}),
     ])
     @patch("client.get_json")
-    def test_org(self, org_name, resp, mock_org):
+    def test_org(self, org_name: str, resp: Dict, mock_org: MagicMock):
         """
          It uses mocking techniques to simulate the behavior of external
          calls and verify the method's logic without actually invoking
          external services.
          """
-        mock_org.return_value = MagicMock(return_value=resp)
+        mock_org.return_value = resp
 
         client = GithubOrgClient(org_name)
         result = client.org
+
+        self.assertEqual(result, resp)
         mock_org.assert_called_once_with(
                 f"https://api.github.com/orgs/{org_name}"
             )
